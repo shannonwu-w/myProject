2fe6bc51b5652a98f2d610402afea80e37cc36d5
1544112010-NB\ShannonWu <wu251117@gmail.com>
2026-02-10 16:38:05 +0800

    0210 加上註解

--- MyProject/server/src/main/java/com/myproject/server/ServerApplication.java
+++ MyProject/server/src/main/java/com/myproject/server/ServerApplication.java
@@ -2,8 +2,10 @@ package com.myproject.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class ServerApplication {
 
 	public static void main(String[] args) {
--- MyProject/server/src/main/java/com/myproject/server/filter/LoginFilter.java
+++ MyProject/server/src/main/java/com/myproject/server/filter/LoginFilter.java
@@ -10,12 +10,21 @@ import javax.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {"/api/*"})
+@WebFilter(urlPatterns = {"/api/admin/*","/api/profile"})
 public class LoginFilter extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws IOException, ServletException {
+
+        String requestURI = request.getRequestURI();
+
+        // 1. 定義白名單：登入 API 不檢查
+        if (requestURI.equals("/api/login")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
         // 檢查 session 中是否有 userCert
         HttpSession session = request.getSession();
         if(session.getAttribute("userCert") == null) { // 沒有憑證

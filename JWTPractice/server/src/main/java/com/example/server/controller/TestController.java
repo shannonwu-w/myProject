32fbd4a116db3850c321bb4cca55d61557c4b9b4
1544112010-NB\ShannonWu <wu251117@gmail.com>
2026-03-02 16:58:06 +0800

    0302 測試api

---
+++ JWTPractice/server/src/main/java/com/example/server/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        System.out.println("API TEST HIT");
+        return "Hello，成功進入網站";
+    }
+
+}
